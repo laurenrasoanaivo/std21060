392b085410ddd166dae8e9c7b9f6d548bd5fadfc
laurenrasoanaivo <hei.lauren.3@gmail.com>
2025-02-05 16:49:40 +0300

    Update HelloWorldController.java

--- src/main/java/com/example/demo/endpoint/rest/controller/health/HelloWorldController.java
+++ src/main/java/com/example/demo/endpoint/rest/controller/health/HelloWorldController.java
@@ -1,4 +1,5 @@
 package com.example.demo.endpoint.rest.controller.health;
+
 import com.example.demo.PojaGenerated;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @AllArgsConstructor
 public class HelloWorldController {
+
   public static final ResponseEntity<String> OK = new ResponseEntity<>("OK", HttpStatus.OK);
   public static final ResponseEntity<String> KO =
       new ResponseEntity<>("KO", HttpStatus.INTERNAL_SERVER_ERROR);
